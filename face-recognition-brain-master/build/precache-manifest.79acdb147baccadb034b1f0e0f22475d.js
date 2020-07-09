ac3977d8593e983faab60381072b79d888fdf49c
hemant-45 <64040781+hemant-45@users.noreply.github.com>
2020-07-09 17:03:50 +0530

    Add files via upload

---
+++ face-recognition-brain-master/build/precache-manifest.79acdb147baccadb034b1f0e0f22475d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698a022fc6ecc1d8ff7bb9476c78422c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa7f15c41c54297bcc24",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0a2ea0403921503760c1",
+    "url": "/static/css/main.cd384c93.chunk.css"
+  },
+  {
+    "revision": "aa7f15c41c54297bcc24",
+    "url": "/static/js/2.a474d44c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a474d44c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a2ea0403921503760c1",
+    "url": "/static/js/main.7517a70b.chunk.js"
+  },
+  {
+    "revision": "c9b0a537ac9221472b6e",
+    "url": "/static/js/runtime-main.30657a9b.js"
+  }
+]);
